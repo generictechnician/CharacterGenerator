4877951dabb9504b035022721f2b0e9f917ce338
generictecnician <generictechnician01@gmail.com>
2017-09-17 20:56:21 +0100

    Add project files.

---
+++ CharacterGenerator/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Character_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rBSpeed = new System.Windows.Forms.RadioButton();
+            this.rBEndurance = new System.Windows.Forms.RadioButton();
+            this.newCharacterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Select Your Highest Stat:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(178, 107);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(93, 54);
+            this.btnGenerate.TabIndex = 20;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.Location = new System.Drawing.Point(13, 54);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 23);
+            this.lblName.TabIndex = 11;
+            this.lblName.Text = "Name:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 24);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(259, 23);
+            this.lblTitle.TabIndex = 13;
+            this.lblTitle.Text = "Character Generator";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Location = new System.Drawing.Point(46, 178);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.Size = new System.Drawing.Size(203, 93);
+            this.rtbOut.TabIndex = 21;
+            this.rtbOut.Text = "";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(57, 54);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(214, 20);
+            this.tbName.TabIndex = 14;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.newCharacterToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(294, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileToolStripMenuItem,
+            this.openFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newFileToolStripMenuItem.Text = "New File";
+            this.newFileToolStripMenuItem.Click += new System.EventHandler(this.newFileToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // rBSpeed
+            // 
+            this.rBSpeed.Checked = true;
+            this.rBSpeed.Location = new System.Drawing.Point(46, 107);
+            this.rBSpeed.Name = "rBSpeed";
+            this.rBSpeed.Size = new System.Drawing.Size(110, 25);
+            this.rBSpeed.TabIndex = 22;
+            this.rBSpeed.TabStop = true;
+            this.rBSpeed.Text = "Speed";
+            this.rBSpeed.UseVisualStyleBackColor = true;
+            // 
+            // rBEndurance
+            // 
+            this.rBEndurance.Location = new System.Drawing.Point(46, 137);
+            this.rBEndurance.Name = "rBEndurance";
+            this.rBEndurance.Size = new System.Drawing.Size(110, 25);
+            this.rBEndurance.TabIndex = 23;
+            this.rBEndurance.Text = "Endurance";
+            this.rBEndurance.UseVisualStyleBackColor = true;
+            // 
+            // newCharacterToolStripMenuItem
+            // 
+            this.newCharacterToolStripMenuItem.Name = "newCharacterToolStripMenuItem";
+            this.newCharacterToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.newCharacterToolStripMenuItem.Text = "New Character";
+            this.newCharacterToolStripMenuItem.Click += new System.EventHandler(this.newCharactorToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 291);
+            this.Controls.Add(this.rBEndurance);
+            this.Controls.Add(this.rBSpeed);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.rtbOut);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximumSize = new System.Drawing.Size(310, 330);
+            this.MinimumSize = new System.Drawing.Size(310, 330);
+            this.Name = "Form1";
+            this.Text = "Character Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.RadioButton rBSpeed;
+        private System.Windows.Forms.RadioButton rBEndurance;
+        private System.Windows.Forms.ToolStripMenuItem newCharacterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ CharacterGenerator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Character_Generator
+{
+    public partial class Form1 : Form
+    {
+        string path = "characters.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            bool[] option;
+            option = new bool[3];
+            option[1] = true;
+            option[2] = false;
+
+            string[] optionName;
+            optionName = new string[3];
+            optionName[1] = "Speed";
+            optionName[2] = "Endurance";
+
+            int divide = 1;
+            int stat = 1;
+
+            Random random = new Random();
+
+
+            //Check if file exits and create if doesn't
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(" ");
+                }
+            }
+
+            //check which checkbox selected
+            if (rBSpeed.Checked == true)
+            {
+                option[1] = true;
+                option[2] = false;
+            }
+            else if (rBEndurance.Checked == true)
+            {
+                option[1] = false;
+                option[2] = true;
+            }
+            else
+            {
+                option[1] = false;
+                option[2] = false;
+            }
+
+            //add name to file
+            rtbOut.AppendText(tbName.Text + Environment.NewLine);
+            File.AppendAllText(path, tbName.Text + Environment.NewLine);
+
+            //generate values
+            for (int i = 1; i <= 2; i++)
+            {
+                divide = random.Next(1, 4);
+                stat = random.Next(5, 20) / divide;
+
+                if (option[i] == true)
+                {
+                    if (stat <= 10)//if prefered stat is <= 10, increase by 10 to make it better
+                    {
+                        stat += 10;
+                    }
+                    
+                }
+                else if (stat == 1)
+                {
+                    stat = 2; //stops bad values for actual code
+                }
+
+                rtbOut.AppendText(optionName[i] + ": " + stat.ToString() + Environment.NewLine);
+                File.AppendAllText(path, optionName[i] + ": " + stat.ToString() + Environment.NewLine);
+            }
+
+            btnGenerate.Enabled = false;
+
+        }
+
+        private void newFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(" ");
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe", path);
+        }
+
+        private void newCharactorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbName.Text = "";
+            btnGenerate.Enabled = true;
+            rtbOut.Text = "";
+        }
+    }
+}
