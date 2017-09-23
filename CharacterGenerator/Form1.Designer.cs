66d6df9578d824330ad1d894733bc50216d61584
generictecnician <generictechnician01@gmail.com>
2017-09-23 11:36:00 +0100

    Edited Form and Released

--- CharacterGenerator/Form1.Designer.cs
+++ CharacterGenerator/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.lblName = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
@@ -38,27 +37,25 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newCharacterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rBSpeed = new System.Windows.Forms.RadioButton();
-            this.rBEndurance = new System.Windows.Forms.RadioButton();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
+            this.numEndurance = new System.Windows.Forms.NumericUpDown();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblEndurance = new System.Windows.Forms.Label();
+            this.showOutputsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEndurance)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(13, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 23);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Select Your Highest Stat:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(178, 107);
+            this.btnGenerate.Location = new System.Drawing.Point(16, 167);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(93, 54);
-            this.btnGenerate.TabIndex = 20;
+            this.btnGenerate.Size = new System.Drawing.Size(266, 51);
+            this.btnGenerate.TabIndex = 5;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
@@ -84,10 +81,13 @@
             // 
             // rtbOut
             // 
-            this.rtbOut.Location = new System.Drawing.Point(46, 178);
+            this.rtbOut.Location = new System.Drawing.Point(46, 224);
+            this.rtbOut.MaxLength = 65535;
             this.rtbOut.Name = "rtbOut";
+            this.rtbOut.ReadOnly = true;
+            this.rtbOut.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
             this.rtbOut.Size = new System.Drawing.Size(203, 93);
-            this.rtbOut.TabIndex = 21;
+            this.rtbOut.TabIndex = 6;
             this.rtbOut.Text = "";
             // 
             // tbName
@@ -95,13 +95,12 @@
             this.tbName.Location = new System.Drawing.Point(57, 54);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(214, 20);
-            this.tbName.TabIndex = 14;
+            this.tbName.TabIndex = 1;
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.newCharacterToolStripMenuItem});
+            this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(294, 24);
@@ -112,7 +111,8 @@
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.newFileToolStripMenuItem,
-            this.openFileToolStripMenuItem});
+            this.openFileToolStripMenuItem,
+            this.showOutputsToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
@@ -131,61 +131,133 @@
             this.openFileToolStripMenuItem.Text = "Open File";
             this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
             // 
-            // newCharacterToolStripMenuItem
+            // numMin
             // 
-            this.newCharacterToolStripMenuItem.Name = "newCharacterToolStripMenuItem";
-            this.newCharacterToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
-            this.newCharacterToolStripMenuItem.Text = "New Character";
-            this.newCharacterToolStripMenuItem.Click += new System.EventHandler(this.newCharactorToolStripMenuItem_Click);
+            this.numMin.Location = new System.Drawing.Point(151, 89);
+            this.numMin.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numMin.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(120, 20);
+            this.numMin.TabIndex = 2;
+            this.numMin.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
-            // rBSpeed
+            // numMax
             // 
-            this.rBSpeed.Checked = true;
-            this.rBSpeed.Location = new System.Drawing.Point(46, 107);
-            this.rBSpeed.Name = "rBSpeed";
-            this.rBSpeed.Size = new System.Drawing.Size(110, 25);
-            this.rBSpeed.TabIndex = 22;
-            this.rBSpeed.TabStop = true;
-            this.rBSpeed.Text = "Speed";
-            this.rBSpeed.UseVisualStyleBackColor = true;
+            this.numMax.Location = new System.Drawing.Point(151, 115);
+            this.numMax.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numMax.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(120, 20);
+            this.numMax.TabIndex = 3;
+            this.numMax.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
-            // rBEndurance
+            // numEndurance
             // 
-            this.rBEndurance.Location = new System.Drawing.Point(46, 137);
-            this.rBEndurance.Name = "rBEndurance";
-            this.rBEndurance.Size = new System.Drawing.Size(110, 25);
-            this.rBEndurance.TabIndex = 23;
-            this.rBEndurance.Text = "Endurance";
-            this.rBEndurance.UseVisualStyleBackColor = true;
+            this.numEndurance.Location = new System.Drawing.Point(151, 141);
+            this.numEndurance.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numEndurance.Name = "numEndurance";
+            this.numEndurance.Size = new System.Drawing.Size(120, 20);
+            this.numEndurance.TabIndex = 4;
+            this.numEndurance.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // lblMin
+            // 
+            this.lblMin.Location = new System.Drawing.Point(43, 91);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(100, 18);
+            this.lblMin.TabIndex = 25;
+            this.lblMin.Text = "Minimum Speed";
+            this.lblMin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMax
+            // 
+            this.lblMax.Location = new System.Drawing.Point(45, 115);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(100, 18);
+            this.lblMax.TabIndex = 26;
+            this.lblMax.Text = "Maximum Speed";
+            this.lblMax.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEndurance
+            // 
+            this.lblEndurance.Location = new System.Drawing.Point(45, 143);
+            this.lblEndurance.Name = "lblEndurance";
+            this.lblEndurance.Size = new System.Drawing.Size(100, 18);
+            this.lblEndurance.TabIndex = 27;
+            this.lblEndurance.Text = "Endurance";
+            this.lblEndurance.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // showOutputsToolStripMenuItem
+            // 
+            this.showOutputsToolStripMenuItem.Name = "showOutputsToolStripMenuItem";
+            this.showOutputsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showOutputsToolStripMenuItem.Text = "Show Outputs";
+            this.showOutputsToolStripMenuItem.Click += new System.EventHandler(this.showOutputsToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 291);
-            this.Controls.Add(this.rBEndurance);
-            this.Controls.Add(this.rBSpeed);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(294, 221);
+            this.Controls.Add(this.lblEndurance);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.numEndurance);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.rtbOut);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.menuStrip1);
-            this.MaximumSize = new System.Drawing.Size(310, 330);
-            this.MinimumSize = new System.Drawing.Size(310, 330);
+            this.MaximumSize = new System.Drawing.Size(310, 370);
+            this.MinimumSize = new System.Drawing.Size(310, 260);
             this.Name = "Form1";
             this.Text = "Character Generator";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEndurance)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblTitle;
@@ -195,9 +267,13 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
-        private System.Windows.Forms.RadioButton rBSpeed;
-        private System.Windows.Forms.RadioButton rBEndurance;
-        private System.Windows.Forms.ToolStripMenuItem newCharacterToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numMax;
+        private System.Windows.Forms.NumericUpDown numEndurance;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblEndurance;
+        private System.Windows.Forms.ToolStripMenuItem showOutputsToolStripMenuItem;
     }
 }
 
--- CharacterGenerator/Form1.cs
+++ CharacterGenerator/Form1.cs
@@ -7,6 +7,7 @@ namespace Character_Generator
     public partial class Form1 : Form
     {
         string filePath = "characters.info";
+        bool OutInfo = false;
 
         public Form1()
         {
@@ -16,36 +17,13 @@ namespace Character_Generator
         private void btnGenerate_Click(object sender, EventArgs e)
         {
             //Vars & Arrays
-            bool[] option;
-            option = new bool[3];
-            option[1] = true;
-            option[2] = false;
-            string[] optionName;
-            optionName = new string[3];
-            optionName[1] = "speed";
-            optionName[2] = "endurance";
-            int[] stat;
-            stat = new int[3];
-            int divide = 1;
-            Random random = new Random();
-
-            //check which checkbox selected
-            if (rBSpeed.Checked == true)
-            {
-                option[1] = true;
-                option[2] = false;
-            }
-            else if (rBEndurance.Checked == true)
-            {
-                option[1] = false;
-                option[2] = true;
-            }
-            else
-            {
-                option[1] = false;
-                option[2] = false;
-            }
-            //Does File Exist?
+            string[] optionName = new string[4];
+            optionName[1] = "minS";
+            optionName[2] = "maxS";
+            optionName[3] = "endu";
+            int[] stat = new int[4];
+            
+            //Does File Exist? if No, Create it!
             if (!File.Exists(filePath))
             {
                 File.WriteAllText(filePath, "");
@@ -53,26 +31,24 @@ namespace Character_Generator
             //add name to file
             rtbOut.AppendText(tbName.Text + Environment.NewLine);
             File.AppendAllText(filePath, "<name>" + tbName.Text +  Environment.NewLine);
-            //generate values
-            for (int i = 1; i <= 2; i++)
+
+            //Get Values
+            stat[1] = (int)numMin.Value;
+            stat[2] = (int)numMax.Value;
+            stat[3] = (int)numEndurance.Value;
+
+            //add stats to file and Out Box
+            for (int i = 1; i < optionName.Length; i++)
             {
-                divide = random.Next(1, 4);
-                stat[i] = random.Next(5, 20) / divide;
-                if (option[i] == true)
-                {
-                    if (stat[i] <= 10)//if prefered stat is <= 10, increase by 10 to make it better
-                    {
-                        stat[i] += 10;
-                    } 
-                }
-                else if (stat[i] == 1)
-                {
-                    stat[i] = 2; //stops bad values for actual code
-                }
                 rtbOut.AppendText(optionName[i] + ": " + stat[i].ToString() + Environment.NewLine);
                 File.AppendAllText(filePath, "<" + optionName[i] + ">" + stat[i].ToString() + Environment.NewLine);
             }
-            btnGenerate.Enabled = false;
+
+            //Reset Values
+            tbName.Text = "";
+            numMin.Value = 1;
+            numMax.Value = 1;
+            numEndurance.Value = 50;
         }
 
         private void newFileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -88,11 +64,18 @@ namespace Character_Generator
             System.Diagnostics.Process.Start("notepad.exe", filePath);
         }
 
-        private void newCharactorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void showOutputsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            tbName.Text = "";
-            btnGenerate.Enabled = true;
-            rtbOut.Text = "";
+            if (OutInfo == false)
+            {
+                this.Height = 370;
+                OutInfo = true;
+            }
+            else
+            {
+                this.Height = 260;
+                OutInfo = false;
+            }
         }
     }
 }
